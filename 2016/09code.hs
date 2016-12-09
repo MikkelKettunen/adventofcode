e97d649505f077bde2926a17bb9e60d3e82920a8
Mikkel Kettunen <mikkel.kettunen@gmail.com>
2016-12-09 21:57:59 +0100

    part 2 day 09

--- 2016/09code.hs
+++ 2016/09code.hs
@@ -22,8 +22,33 @@ decompressRaw s =
         right = dropWhile (/= '(') s
     in left ++ decompress right
 
-main = do
-    l <- readFile "09input.txt"
-    let str =  decompress l
-    putStrLn l 
-    putStrLn . show . length $ str
+
+
+
+
+
+main = readFile "09input.txt" >>= putStrLn . show . decompress2 
+
+decompress2 :: String -> Int
+decompress2 ('(':xs) = decompressRepeat2 xs
+decompress2 []       = 0
+decompress2 s        = decompressRaw2 s
+
+decompressRepeat2 :: String -> Int 
+decompressRepeat2 s = 
+    let left         = takeWhile (/= 'x') s
+        right        = takeWhile (/= ')') . tail $ dropWhile (/= 'x') s
+        len          = read left
+        count        = read right
+        s'           = tail $ dropWhile (/= ')') s
+        repeatString = take len s'
+        s''          = drop len s'
+    in (count * (decompress2 repeatString))  + (decompress2 s'')
+    
+
+decompressRaw2 :: String -> Int 
+decompressRaw2 s = 
+    let left  = takeWhile (/= '(') s
+        right = dropWhile (/= '(') s
+    in length left + decompress2 right
+   
